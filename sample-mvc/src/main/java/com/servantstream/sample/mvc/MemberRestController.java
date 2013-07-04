322ca106bbbc6a580dd65d10f3ef935cecc339cc
jash <jash@excellentiasoftware.com>
2013-07-03 23:57:03 -0500

    Initial import.

---
+++ sample-mvc/src/main/java/com/servantstream/sample/mvc/MemberRestController.java
@@ -0,0 +1,33 @@
+package com.servantstream.sample.mvc;
+
+import java.util.List;
+
+import com.servantstream.sample.domain.Member;
+import com.servantstream.sample.repo.MemberDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/rest/members")
+public class MemberRestController
+{
+    @Autowired
+    private MemberDao memberDao;
+
+    @RequestMapping(method=RequestMethod.GET, produces="application/json")
+    public @ResponseBody List<Member> listAllMembers()
+    {
+        return memberDao.findAllOrderedByName();
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET, produces="application/json")
+    public @ResponseBody Member lookupMemberById(@PathVariable("id") Long id)
+    {
+        return memberDao.findById(id);
+    }
+}
